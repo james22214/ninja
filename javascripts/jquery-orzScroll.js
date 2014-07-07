ab9b5b3414ebc9ef5c238eb1bcd1cffc635fe5cd
本名 < Email>
2014-07-08 00:14:07 +0800

    first commit

---
+++ javascripts/jquery-orzScroll.js
@@ -0,0 +1,33 @@
+;(function($){
+		$.fn.orzScroll = function(setting){
+			var windowCenter = ($(window).height())/2
+			  , orzProperty = {
+					animation : 'fadeInLeft',
+					offset: 0,
+					delay: '0s'
+			}
+		var orzSetting = $.extend(orzProperty,setting);
+		return this.each(function(){
+			$(this).css('opacity','0').addClass('animated');
+			$(this).css('-webkit-animation-delay',orzProperty.delay);
+			$(this).css('animation-delay',orzProperty.delay);
+			var thisClass = $(this)
+			  , thisClassTop = $(this).offset().top
+			  , thisClassBottom = thisClassTop + ($(this).height())+orzProperty.offset
+			  , thisClassBottomOffset =  thisClassBottom + windowCenter - 150
+			  , thisAnimation = orzSetting.animation;
+			$(window).scroll(function(){
+				var currentScroll = $(this).scrollTop()
+				  , currentScrollCenter = currentScroll+($(this).height())/2
+				  , currentScrollBottom = currentScroll+($(this).height());
+				if((currentScrollCenter > thisClassTop)&&(currentScrollCenter < thisClassBottomOffset)){
+					thisClass.addClass(thisAnimation);
+					thisClass.css('opacity','1');
+				}else if((currentScroll > thisClassBottom) || (currentScrollBottom < thisClassTop)){
+					thisClass.removeClass(thisAnimation);
+					thisClass.css('opacity','0');
+				}
+			});
+		});
+	}
+	})(jQuery);
